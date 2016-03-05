88c494e154168c3cdf3f40a444bf595f62af7191
Rob Hirschfeld <rob@rackn.com>
2016-03-05 17:30:48 -0600

    working on readthedocs.org automation

--- conf.py
+++ conf.py
@@ -217,7 +217,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'DigitalRebardoc'
+htmlhelp_basename = 'Digital_Rebar'
 
 # -- Options for LaTeX output ---------------------------------------------
 
