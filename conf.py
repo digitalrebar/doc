ea457c9159971ee20e2139e236caa1255eadfc0e
Greg Althaus <galthaus@austin.rr.com>
2016-05-11 14:01:09 -0500

    Make readme and read the docs root different.

--- conf.py
+++ conf.py
@@ -60,7 +60,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'README'
+master_doc = 'BOOK'
 
 # General information about the project.
 project = u'Digital Rebar'
